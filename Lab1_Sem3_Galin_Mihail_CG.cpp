f6754ef24a32ae5af6381d8f3050303497cb2d2e
Mikhail Galin <mishagalin85@gmail.com>
2021-11-11 11:37:53 +0500

    .

--- Lab1_Sem3_Galin_Mihail_CG.cpp
+++ Lab1_Sem3_Galin_Mihail_CG.cpp
@@ -12,7 +12,7 @@ COLORREF red = RGB(255, 0, 0);
 COLORREF green = RGB(0, 255, 0);
 COLORREF blue = RGB(0, 0, 255);
 
-void line(int x1, int y1, int x2, int y2, COLORREF Color)
+void line(int x1, int y1, int x2, int y2, COLORREF Color = RGB(255, 255, 255))
 {
 	int x = x1;
 	int y = y1;
@@ -25,7 +25,7 @@ void line(int x1, int y1, int x2, int y2, COLORREF Color)
 	}
 }
 
-void drawLine(int x1, int y1, int x2, int y2, COLORREF Color) {
+void drawLine(int x1, int y1, int x2, int y2, COLORREF Color = RGB(255, 255, 255)) {
 	const int deltaX = abs(x2 - x1);
 	const int deltaY = abs(y2 - y1);
 	const int signX = x1 < x2 ? 1 : -1;
@@ -49,16 +49,16 @@ void drawLine(int x1, int y1, int x2, int y2, COLORREF Color) {
 	}
 }
 
-void drawCircle(int x0, int y0, int radius) {
+void drawCircle(int x0, int y0, int radius, COLORREF BorderColor = RGB(255, 255, 255)) {
 	int x = 0;
 	int y = radius;
 	int delta = 1 - 2 * radius;
 	int error = 0;
 	while (y >= 0) {
-		SetPixel(hdc, x0 + x, y0 + y, white);
-		SetPixel(hdc, x0 + x, y0 - y, white);
-		SetPixel(hdc, x0 - x, y0 + y, white);
-		SetPixel(hdc, x0 - x, y0 - y, white);
+		SetPixel(hdc, x0 + x, y0 + y, BorderColor);
+		SetPixel(hdc, x0 + x, y0 - y, BorderColor);
+		SetPixel(hdc, x0 - x, y0 + y, BorderColor);
+		SetPixel(hdc, x0 - x, y0 - y, BorderColor);
 		error = 2 * (delta + y) - 1;
 		if (delta < 0 && error <= 0) {
 			++x;
@@ -77,34 +77,34 @@ void drawCircle(int x0, int y0, int radius) {
 	}
 }
 
-int LineFill(int x, int y, int dir, int PrevXl, int PrevXr, COLORREF BolderColor, COLORREF Color)
+int LineFill(int x, int y, int dir, int PrevXl, int PrevXr, COLORREF BorderColor, COLORREF Color)
 {
 	int xl = x, xr = x, c;
-	do c = GetPixel(hdc, --xl, y); while ((c != BolderColor) && (c != Color));
-	do c = GetPixel(hdc, ++xr, y); while ((c != BolderColor) && (c != Color));
+	do c = GetPixel(hdc, --xl, y); while ((c != BorderColor) && (c != Color));
+	do c = GetPixel(hdc, ++xr, y); while ((c != BorderColor) && (c != Color));
 	xl++; xr--;
 	drawLine(xl, y, xr, y, Color);
 	for (x = xl; x <= xr; x++)
 	{
 		c = GetPixel(hdc, x, y + dir);
-		if ((c != BolderColor) && (c != Color)) x = LineFill(x, y + dir, dir, xl, xr, BolderColor, Color);
+		if ((c != BorderColor) && (c != Color)) x = LineFill(x, y + dir, dir, xl, xr, BorderColor, Color);
 	}
 	for (x = xl; x < PrevXl; x++)
 	{
 		c = GetPixel(hdc, x, y - dir);
-		if ((c != BolderColor) && (c != Color)) x = LineFill(x, y - dir, -dir, xl, xr, BolderColor, Color);
+		if ((c != BorderColor) && (c != Color)) x = LineFill(x, y - dir, -dir, xl, xr, BorderColor, Color);
 	}
 	for (x = PrevXr; x < xr; x++)
 	{
 		c = GetPixel(hdc, x, y - dir);
-		if ((c != BolderColor) && (c != Color)) x = LineFill(x, y - dir, -dir, xl, xr, BolderColor, Color);
+		if ((c != BorderColor) && (c != Color)) x = LineFill(x, y - dir, -dir, xl, xr, BorderColor, Color);
 	}
 	return xr;
 }
 
 int main()
 {
-	srand(time(0));
+	srand(unsigned int(time(0)));
 	int a = 0, circle1X, circle1Y, circle2X, circle2Y, radius;
 	double distance;
 
@@ -114,10 +114,10 @@ int main()
 		circle1Y = 150 + rand() % 200;
 		circle2X = 150 + rand() % 200;
 		circle2Y = 150 + rand() % 200;
-		radius = 60 + rand() % 20;
+		radius = 80 + rand() % 20;
 		distance = sqrt(pow(circle1X - circle2X, 2) + pow(circle1Y - circle2Y, 2));
 
-		if (distance < (2 * radius) && distance > radius) {
+		if (int(distance) <= (2 * radius) && int(distance) > radius) {
 			drawCircle(circle1X, circle1Y, radius);
 			drawCircle(circle2X, circle2Y, radius);
 			LineFill((circle1X + circle2X) / 2, (circle1Y + circle2Y) / 2, 1, (circle1X + circle2X) / 2, (circle1X + circle2X) / 2, white, red);
